765f407fa5312a9a7b10a197e8b4383f1aec3269
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2022-02-16 19:40:18 +0100

    Create busybox-script-installer.sh

---
+++ busybox-script-installer.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -eux
+
+# Script to busybox shell scripts only for commands are missing on the system 
+
+PRG=busybox-script-installer
+DEBUG=0
+
+debug()
+{
+        if [ $DEBUG -eq 1 ] ; then
+                echo -e "DEBUG: $@"
+        else
+                "$@"
+        fi
+}
+
+create_busybox_links()
+{
+# Create busybox links
+BUSYBOX_BINARY=`find $ROOT_DIR -name "busybox*"`
+echo "$BUSYBOX_BINARY"
+BUSYBOX_TMP=$(mktemp -d -p $ROOT_DIR)
+mkdir -p $BUSYBOX_TMP/bin
+
+${BUSYBOX_BINARY} --install -s $BUSYBOX_TMP/bin/
+rsync -l --ignore-existing $BUSYBOX_TMP/bin/* $ROOT_DIR/bin/
+rm -rf $BUSYBOX_TMP
+for l in $ROOT_DIR/bin/*; do
+  if test -h $l; then
+    ln -sf /sbin/busybox-static $l
+  fi
+done
+}
+
+create_busybox_scripts()
+{
+
+local BUSYBOX_FILE_NAME=$1
+
+# Create busybox links
+# https://wiki.debian.org/ReduceDebian
+# make shure to have a working search PATH env
+PATH="$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+ROOT_DIR=/usr/local/bin
+BUSYBOX_BINARY=`PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH command -v $BUSYBOX_FILE_NAME`
+echo "BUSYBOX_BINARY $BUSYBOX_BINARY"
+#BUSYBOX_TMP=$(mktemp -d -p $ROOT_DIR)
+BUSYBOX_TMP=$(mktemp -d -t $PRG-tmp.XXXXXX)
+mkdir -p $BUSYBOX_TMP/bin
+
+${BUSYBOX_BINARY} --install -s $BUSYBOX_TMP/bin/
+rsync -l --ignore-existing $BUSYBOX_TMP/bin/* $ROOT_DIR/bin/
+rm -rf $BUSYBOX_TMP
+for l in $ROOT_DIR/bin/*; do
+  if test -h $l; then
+    ln -sf /sbin/busybox-static $l
+  fi
+done
+}
+
+create_busybox_scripts busybox.static
+create_busybox_scripts busybox-static
+create_busybox_scripts busybox-extras
+create_busybox_scripts busybox.extras
+create_busybox_scripts busybox
